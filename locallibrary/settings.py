e07e119e71a4d53e151f888664238749f38c22f2
mugambi645 <mugambi645@gmail.com>
2021-10-16 23:54:47 +0300

    deployment changes

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -156,3 +156,8 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_PRELOAD = True
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
